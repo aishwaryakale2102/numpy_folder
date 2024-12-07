3045e967ac02997a6944d913f61fefac787e5611
Aishwsrya Nagesh Kale <aishwaryakale7272@gmail.com>
2024-12-07 19:36:03 +0530

    new file of numpy

---
+++ exp1.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# Create a simple NumPy array
+data = np.array([10, 20, 30, 40, 50])
+
+# Perform basic operations
+array_sum = np.sum(data)  # Sum of array elements
+array_mean = np.mean(data)  # Mean of array elements
+array_max = np.max(data)  # Maximum value in the array
+
+# Print results
+print("Array:", data)
+print("Sum of elements:", array_sum)
+print("Mean of elements:", array_mean)
+print("Maximum value:", array_max)
